d4469e4e257f0dadea5c10616b7292570c996eb6
Vinicius <vinicius.alexandre68@gmail.com>
2022-07-01 14:39:49 -0300

    Revert "mock setup"
    This reverts commit af00a782a089ac40e27e0f63bbe3c0fdff019bfb.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,2 @@
-Mox.defmock(WoWData.Api.AccessTokenBehaviourMock, for: WoWData.Api.AccessTokenBehaviour)
-Mox.defmock(WoWData.Api.ClientBehaviourMock, for: WoWData.Api.ClientBehaviour)
-
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(WoWData.Repo, :manual)
