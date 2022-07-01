af00a782a089ac40e27e0f63bbe3c0fdff019bfb
Vinicius <vinicius.alexandre68@gmail.com>
2022-07-01 14:34:50 -0300

    mock setup

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,5 @@
+Mox.defmock(WoWData.Api.AccessTokenBehaviourMock, for: WoWData.Api.AccessTokenBehaviour)
+Mox.defmock(WoWData.Api.ClientBehaviourMock, for: WoWData.Api.ClientBehaviour)
+
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(WoWData.Repo, :manual)
